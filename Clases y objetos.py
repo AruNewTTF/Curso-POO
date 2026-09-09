2f774460f9579504b944cff194c634dfb4bc1f28
Felipe <felipeponce2005@gmail.com>
2026-09-09 19:45:32 -0300

    Introduciendo Clases y Objetos

---
+++ Clases y objetos.py
@@ -0,0 +1,23 @@
+#forma tediosa de armado
+
+#celular1_marca = "samsung"
+#celular2_marca = "apple"
+#celular3_marca = "hwuawei"
+
+#celular1_modelo = "S23"
+#celular2_modelo = "iPhone 15 pro"
+#celular3_modelo = "P20 pro"
+
+#celular1_camaraF = "48MP"
+#celular2_camaraF = "48MP"
+#celular3_camaraF = "12MP"
+
+#como usar las clases
+class Celular():
+    #atributos estaticos de la clase
+    marca = "Samsung"
+    modelo = "S23"
+    camara = "48MP"
+celu1 = Celular
+print(celu1.modelo)
+    
