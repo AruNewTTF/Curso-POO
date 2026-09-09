dfeae1cb0e867747c5b30a4bba3d7cb22a6c1b28
Felipe <felipeponce2005@gmail.com>
2026-09-09 19:59:13 -0300

    agregando funciones/metodos a la clase de Celular

--- Clases y Atributos.py
+++ Clases y Atributos.py
@@ -4,6 +4,11 @@ class Celular():
         self.modelo = modelo
         self.camara = camara
         
+    def llamar(self):
+        print(f"Estas haciendo una llamada desde un {self.modelo}")
+    def cortar(self):
+        print("Cortaste una llamada")
+        
 celu1 = Celular("Samsung", "S23", "48MP")
 
-print(celu1.modelo)
+celu1.llamar()
